b599e879108bd2b81407ff09c16e99778f425a10
Daniel Jones <dcjones@cs.washington.edu>
2014-12-27 21:55:50 -0800

    Resolve scalars using yaml 1.2 core scheme. Fixes #7, fixes #8

--- src/resolver.jl
+++ src/resolver.jl
@@ -13,31 +13,24 @@ const DEFAULT_MAPPING_TAG = "tag:yaml.org,2002:map"
 const default_implicit_resolvers =
     [
          ("tag:yaml.org,2002:bool",
-          r"^(?:yes|Yes|YES|no|No|NO
-            |true|True|TRUE|false|False|FALSE
-            |on|On|ON|off|Off|OFF)$"x),
-
-         ("tag:yaml.org,2002:float",
-          r"^(?:[-+]?(?:[0-9][0-9_]*)\.[0-9_]*(?:[eE][-+][0-9]+)?
-            |\.[0-9_]+(?:[eE][-+][0-9]+)?
-            |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\.[0-9_]*
-            |[-+]?\.(?:inf|Inf|INF)
-            |\.(?:nan|NaN|NAN))$"x),
+          r"^(?:true|True|TRUE|false|False|FALSE)$"x),
 
          ("tag:yaml.org,2002:int",
           r"^(?:[-+]?0b[0-1_]+
-            |[-+]?0[0-7_]+
-            |[-+]?(?:0|[1-9][0-9_]*)
-            |[-+]?0x[0-9a-fA-F_]+
-            |[-+]?[1-9][0-9_]*(?::[0-5]?[0-9])+)$"x),
+            |[-+]? [0-9]+
+            |0o [0-7]+
+            |0x [0-9a-fA-F]+)$"x),
+
+         ("tag:yaml.org,2002:float",
+          r"^(?:[-+]? ( \. [0-9]+ | [0-9]+ ( \. [0-9]* )? ) ( [eE] [-+]? [0-9]+ )?
+            |[-+]? (?: \.inf | \.Inf | \.INF )
+            |\.nan | \.NaN | \.NAN)$"x),
 
          ("tag:yaml.org,2002:merge",
           r"^(?:<<)$"),
 
          ("tag:yaml.org,2002:null",
-          r"^(?: ~
-            |null|Null|NULL
-            | )$"x),
+          r"^(?:~|null|Null|NULL|)$"x),
 
          ("tag:yaml.org,2002:timestamp",
           r"^(\d{4})-    (?# year)
