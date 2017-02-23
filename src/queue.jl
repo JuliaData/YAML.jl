f6d9740287675a1f83817dd123f60ebfa153d32b
Spencer Lyon <spencerlyon2@gmail.com>
2017-02-23 14:10:13 -0500

    BUG: fix previous bad merge

--- src/queue.jl
+++ src/queue.jl
@@ -1,24 +1,7 @@
-# Simple general-purpose queue.
-
-type EmptyQueue <: Exception
-end
-
-type UnderfullQueue <: Exception
-end
-
-type QueueNode{T}
-    value::T
-    next::Union{QueueNode, Void}
-end
-
-
 type Queue{T}
-    front::Union{QueueNode{T}, Void}
-    back::Union{QueueNode{T}, Void}
-    length::UInt64
-
+    data::Vector{T}
     function (::Type{Queue{T}}){T}()
-        new{T}(nothing, nothing, 0)
+        new{T}(Vector{T}())
     end
 end
 
@@ -29,4 +12,3 @@ enqueue!{T}(q::Queue{T}, value::T) = push!(q.data, value)
 # Enqueue into kth place.
 enqueue!{T}(q::Queue{T}, value::T, k::Integer) = insert!(q.data, k+1, value)
 dequeue!(q::Queue) = shift!(q.data)
-
