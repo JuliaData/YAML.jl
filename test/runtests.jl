9114671a3b0d0c989be4e77c332f4848344823fe
Spencer Lyon <spencerlyon2@gmail.com>
2017-03-02 10:31:42 -0500

    BUG: fix bug in tests...

--- test/runtests.jl
+++ test/runtests.jl
@@ -105,7 +105,7 @@ end
 more_constructors = let
     pairs = [("!Cartesian", :Cartesian),
              ("!AR1", :AR1)]
-    Dict{String,Function}([(t, (c, n) -> construct_type_map(s, c, n))
+    Dict{AbstractString,Function}([(t, (c, n) -> construct_type_map(s, c, n))
                            for (t, s) in pairs])
 end
 
