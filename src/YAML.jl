f6662f384ca60063ff14ba16090c6cff93f20152
Koki Fushimi <paalon1936@gmail.com>
2024-06-15 03:04:36 +0900

    Fix the docstring text as pointed out by Gunnar Farnebäck. (#171)
    * Fix the docstring text as pointed out by Gunnar Farnebäck.
    
    * Update src/YAML.jl
    
    Co-authored-by: Kevin Bonham <kevbonham@gmail.com>
    
    * Update src/YAML.jl
    
    Co-authored-by: Kevin Bonham <kevbonham@gmail.com>
    
    ---------
    
    Co-authored-by: Kevin Bonham <kevbonham@gmail.com>

--- src/YAML.jl
+++ src/YAML.jl
@@ -1,20 +1,20 @@
 """
     YAML
 
-A package to treat YAML.
+A package to read and write YAML.
 https://github.com/JuliaData/YAML.jl
 
 Reading:
 
 * `YAML.load` parses the first YAML document of a YAML file as a Julia object.
-* `YAML.load_all` parses the all YAML documents of a YAML file.
-* `YAML.load_file` is same with `YAML.load` except it reads from a file.
-* `YAML.load_all_file` is same with `YAML.load_all` except it reads from a file.
+* `YAML.load_all` parses all YAML documents of a YAML file.
+* `YAML.load_file` is the same as `YAML.load` except it reads from a file.
+* `YAML.load_all_file` is the same as `YAML.load_all` except it reads from a file.
 
 Writing:
 
 * `YAML.write` prints a Julia object as a YAML file.
-* `YAML.write_file` is same with `YAML.write` except it writes to a file.
+* `YAML.write_file` is the same as `YAML.write` except it writes to a file.
 * `YAML.yaml` converts a given Julia object to a YAML-formatted string.
 """
 module YAML
