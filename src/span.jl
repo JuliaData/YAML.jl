a440b9a0e69471fb2a5ed6bad4277322934b7616
Koki Fushimi <paalon1936@gmail.com>
2024-07-01 23:37:46 +0900

    Add show function to spans (#222)
    * Split mark and span from `src/tokens.jl`
    
    * Add iteration and indexing to `Span`.
    
    * Add methods to get start mark and end mark from tokens.
    
    * Replace `token.span.start_mark` and `token.span.end_mark` to
    `firstmark(token)` and `lastmark(token)`.
    
    * Fix existing bugs about getting marks from tokens.
    
    * Replace remained expressions.
    
    * Remove methods for `Span`.
    
    * Add a show function to spans.

--- src/span.jl
+++ src/span.jl
@@ -3,3 +3,5 @@ struct Span
     start_mark::Mark
     end_mark::Mark
 end
+
+show(io::IO, span::Span) = print(io, "(line, column) ∈ (", span.start_mark.line, ", ", span.start_mark.column, ")...(", span.end_mark.line, ", ", span.end_mark.column, ")")
